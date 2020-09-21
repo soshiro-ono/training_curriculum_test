04942080d9fb92bf3ae1b91f538000503f18bac7
小野　総司郎 <ssosksono@gmail.com>
2020-09-21 17:24:16 +0900

    コンフリクト後再編集

--- app/controllers/calendars_controller.rb
+++ app/controllers/calendars_controller.rb
@@ -8,7 +8,6 @@ class CalendarsController < ApplicationController
 
   # 予定の保存
   def create
-    # binding.pry
     Plan.create(plan_params)
     redirect_to action: :index
   end
@@ -42,7 +41,7 @@ class CalendarsController < ApplicationController
       if wday_num >= 7
         wday_num = wday_num - 7
       end
-      days = { :month => (@todays_date + x).month, :date => (@todays_date+x).day, :wday => wdays[wday_num],:plans => today_plans}
+      days = { month: (@todays_date + x).month, date: (@todays_date+x).day, wday: wdays[wday_num],:plans => today_plans}
       @week_days.push(days)
     end
 
